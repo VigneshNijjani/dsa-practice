182fe3020c651250b83cacc15078dd1fd6af1be8
VigneshNijjani <vigneshnijjani@gmail.com>
2026-08-06 21:42:36 +0530

    message

--- day5.py
+++ day5.py
@@ -5,7 +5,13 @@
 # nums=[1,2,3,4,5]
 # solve(nums)
 
-
-l=["",""]
-for i in l:
-    print(l)
+def firstMissingPositive(nums):
+        flag=0
+        for i in range(1,max(nums)):
+            if i not in nums:
+                flag=1
+                print(i)
+        if flag==0:
+            print(max(nums)+1,flag) 
+nums=[3,-4,1,-1]
+firstMissingPositive(nums)
---
+++ day6.py
@@ -0,0 +1,118 @@
+# linked list 
+# ---------------------------------------------------------------------------------------------------------
+
+
+# single linled list 
+
+class single_node:
+    def __init__(self,val,next=None):
+        self.val=val
+        self.next=next
+
+    def __str__(self):
+        return str(self.val)
+
+#inserting data in single linked list
+
+head=single_node(1)
+a=single_node(2)
+b=single_node(3)
+c=single_node(4)
+d=single_node(5)
+
+#passing next node as reference like head->next->next->none
+
+head.next=a
+a.next=b
+b.next=c
+c.next=d
+# ----------------------------------------------------------------------------------------------------------------------------------------
+
+# iterating through linked lists
+
+def display(head):
+    current_node=head
+    while current_node:
+        print(current_node)
+        current_node=current_node.next
+display(head)
+# =====================================================================
+#  or even better display option
+# =====================================================================
+
+def display(head):
+    current_node=head
+    data=[]
+    while current_node:
+        data.append(str(current_node))
+        current_node=current_node.next
+    print(" -> ".join(data))
+display(head)
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+double linked list
+
+class double_linked_list:
+    def __init__(self,val,next=None,prev=None):
+        self.val=val
+        self.next=next
+        self.prev=prev
+
+    def __str__(self):
+        return str(self.val)
+
+##inserting data in double linked list
+
+head=double_linked_list(10)
+a=double_linked_list(20)
+b=double_linked_list(30)
+c=double_linked_list(40)
+
+##passing next node as reference like head<->next<->next<->none
+
+head.next=a
+a.next=b
+a.prev=head
+b.next=c
+b.prev=a
+c.prev=b
+
+
+def display(head):
+    current_node=head
+    data=[]
+    while current_node:
+        data.append(str(current_node))
+        current_node=current_node.next
+    print(" <-> ".join(data))
+display(head)
+
+
+
+
+
+
+# inserting elements in double linked list at head place 
+#                              or 
+# inserting element at head position of double linked lists
+
+head,tail=double_linked_list(10)
+# inserting secound element
+new_node=double_linked_list(20)
+new_node.next(head)
+head.prev(new_node)
+
+def display(head):
+    current_node=head
+    data=[]
+    while current_node:
+        data.append(str(current_node))
+        current_node=current_node.next
+    print(" -> ".join(data))
+display(head)
+
+
+
+
+
