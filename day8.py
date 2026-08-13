cb3f67bcdbc76c0c9cdb3305a2836b631b13e37d
VigneshNijjani <vigneshnijjani@gmail.com>
2026-08-13 19:44:24 +0530

    code

--- day8.py
+++ day8.py
@@ -57,3 +57,19 @@
 #     print("automorphic number")
 # else:
 #     print("not an automorphic number")
+
+
+seen=set()
+double=set()
+l=[1,3,5,9,5,4,3,2,1]
+peak=max(l)
+for i in range(len(l)):
+    if l[i] not in seen:
+        seen.add(l[i])
+    else:
+        double.add(l[i])
+bridge=max(double)
+if bridge:
+    print(bridge)
+else:
+    print(-1)
