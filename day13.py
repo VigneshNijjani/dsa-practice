d60cb22b49c440b920aa8126044ff97676232cf6
VigneshNijjani <vigneshnijjani@gmail.com>
2026-09-01 20:16:53 +0530

    code

--- day13.py
+++ day13.py
@@ -7,8 +7,28 @@ head=node(10)
 head.next=node(20)
 head.next.next=node(30)
 
+# traversal
+
 curr=head
 while curr:
     print(curr.data,end="->")
     curr=curr.next
 print("Null")
+
+# inserting at start 
+
+# accorinding to above program linkedlist till now is 10->20->30->null
+
+new_node=node(0)
+new_node.next=head
+head=new_node
+
+# traversing agin
+while head:
+    print(head.data,end="->")
+    head=head.next
+print("Null")
+
+# now linked list is 0->10->20->30->Null
+
+
