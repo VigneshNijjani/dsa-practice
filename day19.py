79bd1b755b262b825054511ad00e5b3a69d73e05
VigneshNijjani <vigneshnijjani@gmail.com>
2026-09-19 13:25:29 +0530

    code

--- day19.py
+++ day19.py
@@ -1,1 +1,11 @@
-# day19
+def getFinalState(nums,k,multiplier):
+    for i in range(k):
+        x=float("inf")
+        ind=0 
+        for j in range(len(nums)):
+            if nums[j]<x:
+                x=nums[j]
+                ind=j
+        nums[ind]=nums[ind]*multiplier
+    return nums
+print(getFinalState(nums = [2,1,3,5,6], k = 5, multiplier = 2))
